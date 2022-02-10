0eddd06b74f348aef42eac5403f2f4eee54cbaa2
hskimim <hskimim8855@gmail.com>
2022-02-10 23:21:44 +0900

    debug the range to torch.arange

--- sparse_transformer/factorized_attn/strided.py
+++ sparse_transformer/factorized_attn/strided.py
@@ -7,7 +7,7 @@ class StridedFA(FactorizedAttentionMask):
 
     def a_1(self, seq_len):  # A_{i}^(1)
         mask_m = self.init_mask(seq_len)
-        for i in range(mask_m.shape[1]):
+        for i in torch.arange(mask_m.shape[1]):
             start = i + 1
             end = start + self.l
             if end > mask_m.shape[0]:
@@ -19,8 +19,8 @@ class StridedFA(FactorizedAttentionMask):
 
     def a_2(self, seq_len):  # A_{i}^(2)
         mask_m = self.init_mask(seq_len)
-        for i in range(mask_m.shape[1] - self.l - 1):
+        for i in torch.arange(mask_m.shape[1] - self.l - 1):
             start = i + 1 + self.l
-            mod_indices = torch.arange(start, mask_m.shape[0], l)
+            mod_indices = torch.arange(start, mask_m.shape[0], self.l)
             mask_m[mod_indices, i] = 1
         return mask_m.bool()
